81c814106149a5550ca64ac3b4358d8532da6b82
Shubham Utwal <shubhamutwal@playsimple.in>
2017-09-16 13:51:22 +0530

    Updated positions for ftue images

--- src/js/ftue/screenFour.js
+++ src/js/ftue/screenFour.js
@@ -14,7 +14,8 @@ screenFour.prototype = {
         var sprite = this.game.add.sprite(0, 0, 'ftue_4');
         sprite.scale.setTo(window.innerWidth/1440, window.innerHeight/2560);
         var next_button = this.game.add.button(0, 0, 'next_ftue', this.nextScreen);
-        next_button.alignIn(sprite, Phaser.BOTTOM_CENTER, 0, 100);
+        sprite.scale.setTo(0.5, 0.5);
+        next_button.alignTo(sprite, Phaser.BOTTOM_CENTER, 0, 0);
     },
 
     nextScreen: function() {
--- src/js/ftue/screenOne.js
+++ src/js/ftue/screenOne.js
@@ -11,12 +11,11 @@ screenOne.prototype = {
     },
 
     create: function() {
-        var deviceRatio = 1/((window.innerWidth / window.innerHeight));
         var sprite = this.game.add.sprite(0, 0, 'ftue_1');
         sprite.scale.setTo(window.innerWidth/1440, window.innerHeight/2560);
         var next_button = this.game.add.button(0, 0, 'next_ftue', this.nextScreen);
-        sprite.scale.setTo(0.5,0.5)
-        next_button.alignIn(sprite, Phaser.BOTTOM_CENTER, 0, 100);
+        sprite.scale.setTo(0.5, 0.5);
+        next_button.alignTo(sprite, Phaser.BOTTOM_CENTER, 0, 0);
         // next_button.scale.setTo(1/deviceRatio, 1/deviceRatio);
     },
 
