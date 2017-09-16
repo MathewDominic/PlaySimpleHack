e7a90e9aa9ffc7595e5160cb56f2e85f1ee896de
Prashant Kumar <prashant@playsimple.in>
2017-09-16 13:39:44 +0530

    hints done

--- src/js/ftue/screenOne.js
+++ src/js/ftue/screenOne.js
@@ -13,6 +13,7 @@ screenOne.prototype = {
     create: function() {
         var sprite = this.game.add.sprite(0, 0, 'ftue_1');
         var next_button = this.game.add.button(0, 0, 'next_ftue', this.nextScreen);
+        sprite.scale.setTo(0.5,0.5)
         next_button.alignIn(sprite, Phaser.BOTTOM_CENTER, 0, 100);
     },
 
--- src/js/states/preLoad.js
+++ src/js/states/preLoad.js
@@ -13,11 +13,14 @@ preLoad.prototype = {
         if(level){
             this.game.cache['level'] += 1
         } else {
-            this.game.cache['level'] = 1
+            this.game.cache['level'] = 17
         }
+        this.inputMatrix = null
         console.log("sdf",level);
         gameLogic.init(initial_state[this.game.cache['level']]["grid"]);
     },
+
+    inputMatrix: null,
 
     create: function() {
         // var zombieCount = initial_state[this.game.cache['level']]['mummies'];
@@ -28,6 +31,14 @@ preLoad.prototype = {
         var deviceRatio = 1/((window.innerWidth / window.innerHeight))*rows/2;
         var sprite = this.game.add.sprite(0,0, 'bg');
         sprite.scale.setTo(window.innerWidth/1440, window.innerHeight/2560);
+
+        var back = this.game.add.sprite(window.innerWidth/12,window.innerHeight/16, 'back');
+        back.scale.setTo(1 / (deviceRatio/rows*3) , 1 / (deviceRatio/rows*3));
+        var holpos = this.game.add.sprite(window.innerWidth/12+window.innerWidth/4,window.innerHeight/16, 'holpos');
+        holpos.scale.setTo(1 / (deviceRatio/rows*3), 1 / (deviceRatio/rows*3));
+        var timer = this.game.add.sprite(window.innerWidth/12+ window.innerWidth/4 + window.innerWidth/4,window.innerHeight/16, 'timer');
+        timer.scale.setTo(1 / (deviceRatio/rows*3), 1 / (deviceRatio/rows*3));
+
         var zombie = this.game.add.sprite(window.innerWidth/4,window.innerHeight/12, 'zombie');
         zombie.scale.setTo(1 / (deviceRatio/rows*3) , 1 / (deviceRatio/rows*3));
         var ghost = this.game.add.sprite(window.innerWidth/4+window.innerWidth/6,window.innerHeight/12, 'ghost');
@@ -53,13 +64,17 @@ preLoad.prototype = {
         };
         var logo = [];
         var xOff, yOff;
-        var inputMatrix = initial_state[this.game.cache['level']]["grid"];
+        var inputMatrix = JSON.parse(JSON.stringify(initial_state[this.game.cache['level']]["grid"]));
         for(var i=0;i<inputMatrix.length;i++) {
             for(var j=0;j<inputMatrix[i].length;j++) {
                 if(!((inputMatrix[i][j] == '/') || (inputMatrix[i][j] == '\\')))
-                    inputMatrix[i][j]='B';
+                    inputMatrix[i][j]=this.inputMatrix ? this.inputMatrix[i][j] : 'E'
             }
         }
+        if(!this.inputMatrix){
+            this.inputMatrix = inputMatrix
+        }
+        this.isRotateEnabled = false
         console.log("asf",inputMatrix);
         var countsArr = gameLogic.getCount();
         for(var i=0;i<cols+2;i++) {
@@ -89,14 +104,36 @@ preLoad.prototype = {
                     text.anchor.setTo(0, 0);
                 } else {
                     var cellSprite;
-                    if(initial_state[this.game.cache['level']]["grid"][j-1][i-1] == '/') {
+                    if(this.showCorrect && this.showCorrect[0] == j-1 && this.showCorrect[1] == i-1){
+                        var sprite = this.game.add.sprite(138 / deviceRatio * (i - 1) + xOff, 138 / deviceRatio * (j - 1) + yOff, initial_state[this.game.cache['level']]["grid"][j-1][i-1].toLowerCase());
+                        sprite.scale.setTo(1/deviceRatio, 1/deviceRatio);
+                        sprite.inputEnabled = true;
+                        sprite.events.onInputDown.add(onDown.bind(this,j,i,inputMatrix), this);
+                        sprite.anchor.setTo(0, 0);
+                        this.inputMatrix[j-1][i-1] = initial_state[this.game.cache['level']]["grid"][j-1][i-1]
+                        inputMatrix[j-1][i-1] = initial_state[this.game.cache['level']]["grid"][j-1][i-1]
+                        this.showCorrect = null
+                    } else if(this.showWrong && this.showWrong[0] == j-1 && this.showWrong[1] == i-1){
+                        var sprite = this.game.add.sprite(138 / deviceRatio * (i - 1) + xOff, 138 / deviceRatio * (j - 1) + yOff, 'back');
+                        sprite.scale.setTo(1/deviceRatio, 1/deviceRatio);
+                        sprite.inputEnabled = true;
+                        sprite.events.onInputDown.add(onDown.bind(this,j,i,inputMatrix), this);
+                        sprite.anchor.setTo(0, 0);
+                        this.showWrong = null
+                    } else if(initial_state[this.game.cache['level']]["grid"][j-1][i-1] == '/') {
                         var sprite = this.game.add.sprite(138 / deviceRatio * (i - 1) + xOff, 138 / deviceRatio * (j - 1) + yOff, 'f');
-                        sprite.scale.setTo(1 / deviceRatio, 1 / deviceRatio);
+                        sprite.scale.setTo(1/deviceRatio, 1/deviceRatio);
+                        sprite.inputEnabled = true;
+                        sprite.events.onInputDown.add(rotate.bind(this,j,i,initial_state[this.game.cache['level']]["grid"]), this);
+                        sprite.anchor.setTo(0, 0);
                     } else if(initial_state[this.game.cache['level']]["grid"][j-1][i-1] == '\\') {
                         var sprite = this.game.add.sprite(138 / deviceRatio * (i - 1) + xOff, 138 / deviceRatio * (j - 1) + yOff, 'b');
-                        sprite.scale.setTo(1 / deviceRatio, 1 / deviceRatio);
+                        sprite.scale.setTo(1/deviceRatio, 1/deviceRatio);
+                        sprite.inputEnabled = true;
+                        sprite.anchor.setTo(0, 0);
+                        sprite.events.onInputDown.add(rotate.bind(this,j,i,initial_state[this.game.cache['level']]["grid"]), this);
                     } else {
-                        var sprite = this.game.add.sprite(138 / deviceRatio * (i - 1) + xOff, 138 / deviceRatio * (j - 1) + yOff, 'e');
+                        var sprite = this.game.add.sprite(138 / deviceRatio * (i - 1) + xOff, 138 / deviceRatio * (j - 1) + yOff, inputMatrix[j-1][i-1].toLowerCase());
                         sprite.scale.setTo(1 / deviceRatio, 1 / deviceRatio);
                         sprite.inputEnabled = true;
                         sprite.events.onInputDown.add(onDown.bind(this,j,i,inputMatrix), this);
@@ -104,28 +141,88 @@ preLoad.prototype = {
                     }
                 }
             }
-            console.log(gameLogic.getCount());
-//                logo.push(row);
+        }
+
+        var show_error = this.game.add.sprite(window.innerWidth/4,7*window.innerHeight/8, 'se');
+        show_error.scale.setTo(1 / (deviceRatio/rows*3) , 1 / (deviceRatio/rows*3));
+        show_error.inputEnabled = true;
+        show_error.events.onInputDown.add(showError, this);
+
+        var show_monster = this.game.add.sprite(window.innerWidth/4+window.innerWidth/6,7*window.innerHeight/8, 'sm');
+        show_monster.scale.setTo(1 / (deviceRatio/rows*3), 1 / (deviceRatio/rows*3));
+        show_monster.inputEnabled = true;
+        show_monster.events.onInputDown.add(showMonster, this);
+
+        var turn_mirror = this.game.add.sprite(window.innerWidth/4+ window.innerWidth/6 + window.innerWidth/6,7*window.innerHeight/8, 'tm');
+        turn_mirror.scale.setTo(1 / (deviceRatio/rows*3), 1 / (deviceRatio/rows*3));
+        turn_mirror.inputEnabled = true;
+        turn_mirror.events.onInputDown.add(turnMiror, this);
+        
+        function turnMiror(sprite, pointer) {
+            this.isRotateEnabled = true
+        }
+        function showMonster(sprite, pointer){
+            var origin = gameLogic.gridPayload
+            for (var i=0; i<this.inputMatrix.length; i++){
+                for (var j=0; j<this.inputMatrix[i].length; j++){
+                    if((origin[i][j] == 'Z' || origin[i][j] == 'V' || origin[i][j] == 'G') && (origin[i][j] != this.inputMatrix[i][j])){
+                        this.showCorrect = [i,j]
+                        break
+                    }
+                }
+            }
+            this.create()
+        }
+        function showError(sprite, pointer){
+            var wrongs = gameLogic.getWrong(this.inputMatrix)
+            var limit = wrongs.length
+            var ele = Math.floor((Math.random() * 100) + 1) % limit
+            this.showWrong = wrongs[ele]
+            this.create()
+        }
+        function rotate(i,j,inputMatrix,sprite, pointer) {
+            if(!this.isRotateEnabled){
+                return
+            }
+            if(inputMatrix[i-1][j-1] == '\\'){
+                inputMatrix[i-1][j-1] = '/'
+                sprite.loadTexture('f')
+            } else if(inputMatrix[i-1][j-1] == '/'){
+                inputMatrix[i-1][j-1] = '\\'
+                sprite.loadTexture('b')
+            }
+            this.isRotateEnabled = false
+            // this.isEnabled = false
+            gameLogic.init(inputMatrix)
+            this.create()
         }
 
         function onDown(i,j,inputMatrix,sprite, pointer) {
             console.log(i,j);
-
+            this.isRotateEnabled = false
             if (sprite.key === 'e') {
                 sprite.loadTexture('z');
                 inputMatrix[i-1][j-1] = 'Z'
+                this.inputMatrix[i-1][j-1] = 'Z'
             }
             else if (sprite.key === 'z') {
                 sprite.loadTexture('g');
                 inputMatrix[i-1][j-1] = 'G'
+                this.inputMatrix[i-1][j-1] = 'G'
             }
             else if (sprite.key === 'g') {
                 sprite.loadTexture('v');
                 inputMatrix[i-1][j-1] = 'V'
+                this.inputMatrix[i-1][j-1] = 'V'
             }
             else if (sprite.key === 'v') {
                 sprite.loadTexture('e');
-                inputMatrix[i-1][j-1] = 'B'
+                inputMatrix[i-1][j-1] = 'E'
+                this.inputMatrix[i-1][j-1] = 'E'
+            } else{
+                sprite.loadTexture('e');
+                inputMatrix[i-1][j-1] = 'E'
+                this.inputMatrix[i-1][j-1] = 'E'
             }
             if(gameLogic.isWin(inputMatrix)){
                 this.game.state.start("Preload")
