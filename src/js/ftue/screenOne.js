6dabd31f8e3755f88324b769911ab93048f408c1
Shubham Utwal <shubhamutwal@playsimple.in>
2017-09-16 14:36:21 +0530

    Realigned

--- src/js/ftue/screenOne.js
+++ src/js/ftue/screenOne.js
@@ -12,10 +12,13 @@ screenOne.prototype = {
 
     create: function() {
 
+        var deviceRatio = 1/((window.innerWidth / window.innerHeight));
+
         var sprite = this.game.add.sprite(0, 0, 'ftue_1');
         sprite.scale.setTo(window.innerWidth/1440, window.innerHeight/2560);
         sprite.alignIn(this.game.world.bounds, Phaser.TOP_CENTER);
-        var next_button = this.game.add.button((this.game.world.width)/4, (2.3*this.game.world.height)/3, 'next_ftue', this.nextScreen);
+        var next_button = this.game.add.button((this.game.world.width)/3, (2.4*this.game.world.height)/3, 'next_ftue', this.nextScreen);
+        next_button.scale.setTo(1/4*deviceRatio, 1/4*deviceRatio);
     },
 
     nextScreen: function() {
